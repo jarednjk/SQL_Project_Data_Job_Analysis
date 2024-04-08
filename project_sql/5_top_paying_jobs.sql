ec3fe4db2d32e6b99ee3ccaf6fad32c7e4e572b1
Jared Ng <jarednjk@gmail.com>
2024-04-08 12:05:00 +0800

    most in-demand skills for data analyst in singapore

---
+++ project_sql/5_top_paying_jobs.sql
@@ -0,0 +1,17 @@
+SELECT
+    skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT(skills_job_dim.job_id) AS demand_count,
+    ROUND(AVG(salary_year_avg), 0) AS average_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' AND 
+    salary_year_avg IS NOT NULL AND
+    job_location = 'Singapore'
+GROUP BY
+    skills_dim.skill_id
+ORDER BY 
+    demand_count DESC,
+    average_salary DESC;

3f7039d53e02fb0d8ab6cebb4bc52e63be3ae2b4
Jared Ng <jarednjk@gmail.com>
2024-04-08 11:26:13 +0800

    top skills based on average salary of data analyst in Singapore

---
+++ project_sql/4_top_paying_jobs.sql
@@ -0,0 +1,14 @@
+SELECT
+    skills,
+    ROUND(AVG(salary_year_avg), 0) AS average_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' AND 
+    salary_year_avg IS NOT NULL AND
+    job_location = 'Singapore'
+GROUP BY
+    skills
+ORDER BY
+    average_salary DESC;

adfcde94e4f3928f58f4f65d01c8e6d02af73fa0
Jared Ng <jarednjk@gmail.com>
2024-04-08 13:39:15 +0800

    count to see which is the most in-demand  skill for top paying jobs in sg

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -1,4 +1,3 @@
-
 WITH top_paying_jobs AS (
     SELECT
         job_id,
@@ -29,3 +28,34 @@ INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 ORDER BY
     salary_year_avg DESC;
+
+WITH top_paying_jobs AS (
+    SELECT
+        job_id,
+        job_title,
+        job_location,
+        salary_year_avg,
+        job_posted_date,
+        name AS company_name
+    FROM 
+        job_postings_fact
+    LEFT JOIN 
+        company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE 
+        job_title_short='Data Analyst' AND
+        job_location = 'Singapore' AND
+        salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT 
+    skills,
+    COUNT(*)
+FROM 
+    top_paying_jobs
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+GROUP BY skills
+ORDER BY COUNT(*) DESC;
